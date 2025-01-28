4f9fd33a35c6a83861f4680b9e222463ab73c80b
ashish-sng <ashishsingh.1899@gmail.com>
2025-01-28 10:39:52 +0530

    fix: service-worker path fix

--- src/services/offlineService.js
+++ src/services/offlineService.js
@@ -2,7 +2,7 @@ export const registerServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('../../public/service-worker.js')
+        .register('/service-worker.js')
         .then((registration) => {
           console.log(
             'Service Worker registered with scope:',
